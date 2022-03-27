a9ed766a25e57be471a826a72ca8b129a3f5677d
Guilherme Manhani <guilherme_manhani@hotmail.com>
2022-03-27 11:30:22 -0300

    move ValidationComposite test setup to a helper method

--- test/validation/validators/validation_composite_test.dart
+++ test/validation/validators/validation_composite_test.dart
@@ -21,19 +21,38 @@ void main() {
   late ValidationComposite sut;
   late MockFieldValidation validation1;
   late MockFieldValidation validation2;
+  late MockFieldValidation validation3;
+
+  void mockValidation1(String? error) {
+    when(validation1.validate(any)).thenReturn(error);
+  }
+
+  void mockValidation2(String? error) {
+    when(validation2.validate(any)).thenReturn(error);
+  }
+
+  void mockValidation3(String? error) {
+    when(validation3.validate(any)).thenReturn(error);
+  }
 
   setUp(() {
     validation1 = MockFieldValidation();
     when(validation1.field).thenReturn('any_field');
-    when(validation1.validate(any)).thenReturn(null);
+    mockValidation1(null);
 
     validation2 = MockFieldValidation();
     when(validation2.field).thenReturn('any_field');
-    when(validation2.validate(any)).thenReturn('');
+    mockValidation2(null);
 
-    sut = ValidationComposite([validation1, validation2]);
+    validation3 = MockFieldValidation();
+    when(validation3.field).thenReturn('any_field');
+    mockValidation3(null);
+
+    sut = ValidationComposite([validation1, validation2, validation3]);
   });
   test('Shold return null if all validations returns null or empty', () {
+    mockValidation2('');
+
     final error = sut.validate(field: 'any_field', value: 'any_value');
 
     expect(error, null);
