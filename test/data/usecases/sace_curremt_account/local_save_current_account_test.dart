43123fa21dc1dbe34664d4388d65c347f86c40e8
Guilherme Manhani <gmanhani@brq.com>
2022-04-04 15:15:54 -0300

    move production code to their own files

--- test/data/usecases/sace_curremt_account/local_save_current_account_test.dart
+++ test/data/usecases/sace_curremt_account/local_save_current_account_test.dart
@@ -1,30 +1,13 @@
 import 'package:faker/faker.dart';
+import 'package:flutter_clean_solid_tdd_designpatterns/data/cache/save_secure_cache_storage.dart';
+import 'package:flutter_clean_solid_tdd_designpatterns/data/usecases/usecases.dart';
 import 'package:flutter_clean_solid_tdd_designpatterns/domain/entities/account_entity.dart';
 import 'package:flutter_clean_solid_tdd_designpatterns/domain/helpers/helpers.dart';
-import 'package:flutter_clean_solid_tdd_designpatterns/domain/usecases/save_current_account.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 
 import 'local_save_current_account_test.mocks.dart';
-
-class LocalSaveCurrentAccount implements SaveCurrentAccount {
-  final SaveSecureCacheStorage saveSecureCacheStorage;
-  LocalSaveCurrentAccount({required this.saveSecureCacheStorage});
-  @override
-  Future<void> save(AccountEntity account) async {
-    try {
-      await saveSecureCacheStorage.saveSecure(
-          key: 'token', value: account.token);
-    } catch (error) {
-      throw DomainError.unexpected;
-    }
-  }
-}
-
-abstract class SaveSecureCacheStorage {
-  Future<void> saveSecure({required String key, required String value});
-}
 
 @GenerateMocks([SaveSecureCacheStorage])
 void main() {
